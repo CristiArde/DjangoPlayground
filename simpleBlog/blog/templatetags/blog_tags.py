f531a2bc3a2da50aae0f3760ca8c30360a5b40a3
CristiArde <c_burns93@yahoo.ca>
2021-04-02 15:22:16 +0200

    created a include template filter, gd for custom components

--- simpleBlog/blog/templatetags/blog_tags.py
+++ simpleBlog/blog/templatetags/blog_tags.py
@@ -3,7 +3,14 @@ from ..models import Post
 
 register = template.Library()
 
-#@register.simple_tag(name='my_tag') to give it a diffrent name
+
+# @register.simple_tag(name='my_tag') to give it a diffrent name
 @register.simple_tag
 def total_posts():
     return Post.published.count()
+
+
+@register.inclusion_tag('blog/post/latest_post.html')
+def show_latest_posts(count=5):
+    latest_posts = Post.published.order_by('-publish')[:count]
+    return {'latest_posts': latest_posts}
